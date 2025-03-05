6d4ffc49251eef08425c4f65cc97e29bcc0dbc3d
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-03-05 13:30:47 +0530

    [Abhay Shrivastava] Add . validation feature for address book fields

--- src/addressBookSystem.js
+++ src/addressBookSystem.js
@@ -1,12 +1,79 @@
 class AddressBookSystem{
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+        this.firstName = this.validateName(firstName);
+        this.lastName = this.validateName(lastName);
+        this.address = this.validateAddress(address);
+        this.city = this.validateCity(city);
+        this.state = this.validateState(state);
+        this.zip = this.validateZip(zip);
+        this.phoneNumber = this.validatePhoneNumber(phoneNumber);
+        this.email = this.validateEmail(email);
     }
+
+    validateName(name){
+        const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
+        if(!nameRegex.test(name)){
+            throw new Error("Invalid name: Name should start with Capital and Minimum 3 Characters");
+        }
+        return name;
+    }
+    
+    validateAddress(address){
+        const baseRegex = /^[A-Za-z0-9\s]{4,}$/;
+        if(!baseRegex.test(address)){
+            throw new Error("Invalid Address: Address should contain atleast 4 characters");
+        }
+        return address;
+    }
+
+    validateState(state){
+        const baseRegex = /^[A-Za-z0-9\s]{4,}$/;
+        if(!baseRegex.test(state)){
+            throw new Error("Invalid State: State should contain atleast 4 characters");
+        }
+        return state;
+    }
+
+    validateCity(city){
+        const baseRegex = /^[A-Za-z0-9\s]{4,}$/;
+        if(!baseRegex.test(city)){
+            throw new Error("Invalid City: City should contain atleast 4 characters");
+        }
+        return city;
+    }
+
+    validateZip(zip){
+        const zipRegex = /^[1-9][0-9]{5}$/;
+        if(!zipRegex.test(zip)){
+            throw new Error("Invalid zip: Enter a valid zip");
+        }
+        return zip;
+    }
+    validatePhoneNumber(phoneNumber){
+        const phoneNumberRegex = /^\+?([0-9]{1,3}[-. ])?[0-9]{10}$/;
+        if(!phoneNumberRegex.test(phoneNumber)){
+            throw new Error("Invalid Phone Number");
+        }
+        return phoneNumber;
+    }
+
+    validateEmail(email){
+        const emailRegex = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+        if(!emailRegex.test(email)){
+            throw new Error("Invalid Email Address");
+        }
+        return email;
+    }
+
+    toString() {
+        return `firstname: ${this.firstName}, lastname: ${this.lastName}, state: ${this.state}, city: ${this.city}, address: ${this.address}, zip: ${this.zip}, phone number: ${this.phoneNumber}, email: ${this.email}`;
+    }
+
+}
+
+try{
+    let addressBook1 = new AddressBookSystem("Abhay", "Shrivastava", "Rajiv Nagar 123", "Bhopal", "Madhya Pradesh", 462021, "+91-6265581172", "abhay123@gmail.com");
+    console.log(addressBook1.toString());
+} catch(error){
+    console.error(error.message);
 }
